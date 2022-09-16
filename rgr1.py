b48e2989ded59037c62d103be27343244a89b4e4
max <makskms1@gmail.com>
2022-09-16 23:57:56 +1000

    rgr1.py

--- rgr1.py
+++ rgr1.py
@@ -6,42 +6,99 @@ class App(QtWidgets.QApplication):
         super().__init__(sys.argv)
         self.window = Window(self)
         self.window.show()
-     
-class Window(QtWidgets.QWidget):
-    def __init__(self, app):
+
+class Window(QtWidgets.QWidget):    
+    def __init__(self):
         super().__init__()
-        self.setWindowTitle("Первая программа на PyQt")
+        self.setWindowTitle("букашка управляемая слайдерами")
         self.resize(300, 200)
-        
-        vSlider = QtWidgets.QSlider()
-        hSlider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
-        
-        label = QtWidgets.QLabel()
 
-
-        pix = QtGui.QPixmap('ic_bug.png')
+        self.vSlider = QtWidgets.QSlider()
+        self.hSlider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
         
-        pix = pix.scaled(QtCore.QSize(100, 100), QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
-
+        self.vSlider.setInvertedAppearance(True)
         
-        painter = QtGui.QPainter()
-
-        painter.drawPixmap(100, 100, pix)
-
+        self.vSlider.valueChanged.connect(self.on_vSliderValueChanged)
+        self.hSlider.valueChanged.connect(self.on_hSliderValueChanged)
         
+        self.field = Field()
+        
+        self.hSlider.setRange(0, self.field.size().width() - 50)
+        self.vSlider.setRange(0, self.field.size().height() - 50)
 
         main_layout = QtWidgets.QHBoxLayout()  
         left_vBox = QtWidgets.QVBoxLayout()
-        left_vBox.addWidget(vSlider)
+        left_vBox.addWidget(self.vSlider)
         
         right_vBox = QtWidgets.QVBoxLayout()
-        right_vBox.addWidget(label)
-        right_vBox.addWidget(hSlider)
+        right_vBox.addWidget(self.field)
+        right_vBox.addWidget(self.hSlider)
                
         main_layout.addLayout(left_vBox)
         main_layout.addLayout(right_vBox)
-        
+
         self.setLayout(main_layout)
+        
+    def resizeEvent(self, event):
+        self.set_sliders_range()
+        return super(Window, self).resizeEvent(event)
+    
+    def set_sliders_range(self):
+        self.hSlider.setMaximum(self.field.size().width() - self.field.image_size.width())
+        self.vSlider.setMaximum(self.field.size().height() - self.field.image_size.height())
+        
+    def on_hSliderValueChanged(self):
+        self.field.create_image(self.hSlider.value(), self.field.y)
+        
+    def on_vSliderValueChanged(self):
+        self.field.create_image(self.field.x, self.vSlider.value())
+        
+class Field(QtWidgets.QLabel):
+    def __init__(self):
+        super().__init__()
+        self.x = 0
+        self.y = 0
+        
+        self.direction = "right"
+        
+        self.image_size = QtCore.QSize(50, 50)
+        
+        self.pix = QtGui.QPixmap('ic_bug.png')
+        self.pix = self.pix.scaled(self.image_size.width(), 
+                                   self.image_size.height())  
+    
+    def create_image(self, x, y):
+        self.set_direction(x, y)
+        
+        self.x = x
+        self.y = y
+        
+        self.update()
+
+    def paintEvent(self, event):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.drawPixmap(qp, self.x, self.y)
+        qp.end()
+        
+    def drawPixmap(self, qp, x, y):   
+        transform = QtGui.QTransform()
+        transform.rotate(self.get_degree())
+        
+        pt = self.pix.transformed(transform)
+        qp.drawPixmap(x, y, pt)
+        
+    def get_degree(self):
+        if (self.direction == "right"): return 0
+        if (self.direction == "left"): return 180
+        if (self.direction == "up"): return 90
+        if (self.direction == "down"): return 270
+        
+    def set_direction(self, x, y):
+        if (x > self.x): self.direction = "right"
+        if (x < self.x): self.direction = "left"
+        if (y > self.y): self.direction = "up"
+        if (y < self.y): self.direction = "down"  
 
 if __name__ == "__main__":
     app = App()
--- test.py
+++ test.py
@@ -1,51 +1,37 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtGui import QPainter, QColor, QFont, QPixmap
 from PyQt5.QtCore import Qt
-
+from PyQt5 import QtWidgets 
 
 class Example(QWidget):
-
     def __init__(self):
         super().__init__()
 
         self.initUI()
 
-        self.x = 0
-        self.y = 0
-
     def initUI(self):
         self.setGeometry(300, 300, 280, 170)
         self.setWindowTitle('Draw text')
+        
+        self.field = Field()
+        
+        vbox = QtWidgets.QVBoxLayout()
+        vbox.addWidget(self.field)
+            
+        for i in range(10):
+            self.field.create_image(i * 10, 0)
+    
+        self.setLayout(vbox)
         self.show()
 
-    def create_image(self, x, y):
-        self.x = x
-        self.y = y
-        
-        self.update()
-
-    def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        self.drawPixmap(event, qp, self.x, self.y)
-        qp.end()
-        
-    def drawPixmap(self, event, qp, x, y):
-        p = QPixmap('ic_bug.png')
-        p = p.scaled(50, 50)
-        qp.drawPixmap(x, y, p)
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = Example()
-    
-    for i in range(10):
-        ex.create_image(i * 10, 0)
     
     sys.exit(app.exec_())
